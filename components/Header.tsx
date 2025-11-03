d9ca9f3594791aad95b2b754e4424b13590969dd
X0r3 <r3vzone@gmail.com>
2025-11-03 21:14:33 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,12 +6,21 @@ export const Header: React.FC = () => {
   return (
     <header className="py-6 container mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center">
-        <LogoIcon className="h-10 w-auto" />
+        <img
+          src="https://i.ibb.co/YBntfXQm/logo-digital-K-2.png"
+          alt="EthnAfrika Logo"
+          className="h-10 w-auto"
+        />
         <div className="ml-4">
-          <h1 className="text-2xl font-bold tracking-wider text-yellow-400">EthnAfrika</h1>
-          <p className="text-sm text-zinc-400">The Sound of Africa, Worldwide</p>
+          <h1 className="text-2xl font-bold tracking-wider text-yellow-400">
+            EthnAfrika
+          </h1>
+          <p className="text-sm text-zinc-400">
+            The Sound of Africa, Worldwide
+          </p>
         </div>
       </div>
     </header>
   );
 };
+
