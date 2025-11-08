0f41470f2a9fb9547c8c65cf049832029d208cbe
X0r3 <r3vzone@gmail.com>
2025-11-08 22:09:42 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,9 +15,10 @@ export const Header: React.FC = () => {
         {/* Title & Description */}
         <div className="sm:ml-6 mt-4 sm:mt-0">
           <h1 className="text-3xl font-extrabold tracking-widest text-yellow-400 uppercase">
-            Dive into the vibrant essence of the African continent with us.
+            EthnAfrika WebRadio
           </h1>
           <p className="mt-2 text-sm sm:text-base text-zinc-400 leading-relaxed max-w-2xl">
+            Dive into the vibrant essence of the African continent with us.
            <strong> EthnAfrika</strong> — the webradio that celebrates the diversity of Afro-descendant sounds & cultures.  
             From the spiritual energy of Reggae to the hypnotic pulses of Tribal, through the warmth of African House, the ancestral beats of Drums, the modern edge of Afro Pop, the irresistible Afro World & Afro Digital, and the depth of traditional music.  
             <br />
